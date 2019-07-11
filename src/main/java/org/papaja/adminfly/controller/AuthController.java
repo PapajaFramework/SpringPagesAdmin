e475c9cc54008a289304dd4ef575a7f5cc914811
Ivan <ivan.gontarenko@gmail.com>
2019-07-11 17:06:07 +0300

    upd

--- src/main/java/org/papaja/adminfly/controller/AuthController.java
+++ src/main/java/org/papaja/adminfly/controller/AuthController.java
@@ -1,13 +1,22 @@
 package org.papaja.adminfly.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Locale;
 
 @SuppressWarnings({"unused"})
 @Controller
 @RequestMapping("/auth")
 public class AuthController {
+
+    @Autowired
+    private MessageSource messages;
 
     @RequestMapping("/")
     public String home() {
@@ -22,8 +31,6 @@ public class AuthController {
 
     @PreAuthorize("isAuthenticated()")
     @RequestMapping("/logout")
-    public void logout() {
-
-    }
+    public void logout() { }
 
 }
