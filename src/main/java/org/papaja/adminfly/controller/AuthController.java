0f310ffa59774657de4a72f13a52b9499741a7f6
Ivan <ivan.gontarenko@gmail.com>
2019-07-25 17:22:49 +0300

    upd

--- src/main/java/org/papaja/adminfly/controller/AuthController.java
+++ src/main/java/org/papaja/adminfly/controller/AuthController.java
@@ -1,28 +1,29 @@
 package org.papaja.adminfly.controller;
 
+import org.papaja.adminfly.dto.security.UserDto;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @SuppressWarnings({"unused"})
 @Controller
 @RequestMapping("/auth")
-public class AuthController {
+public class AuthController extends AbstractController {
 
     @RequestMapping("/")
-    public String home() {
-        return "redirect:/auth/login";
+    public RedirectView home() {
+        return newRedirect("login");
     }
 
     @PreAuthorize("isAnonymous()")
     @RequestMapping(value = "/login")
-    public String login() {
+    public String login(UserDto user) {
         return "login/login";
     }
 
     @PreAuthorize("isAuthenticated()")
     @RequestMapping("/logout")
-
     public void logout() { }
 
 }
