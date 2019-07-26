752bc2bf1d133353126c745c56a4aca666415b9b
Ivan <ivan.gontarenko@gmail.com>
2019-07-26 18:25:06 +0300

    upd

--- src/main/java/org/papaja/adminfly/core/security/Base64Codec.java
+++ src/main/java/org/papaja/adminfly/core/security/Base64Codec.java
@@ -4,20 +4,14 @@ import org.apache.commons.codec.binary.Base64;
 
 public class Base64Codec implements Encoder<String>, Decoder<String> {
 
-    private Base64 base64;
-
-    public Base64Codec() {
-        this.base64 = new Base64();
-    }
-
     @Override
     public String decode(byte[] bytes) {
-        return new String(base64.decode(bytes));
+        return new String(Base64.decodeBase64(bytes));
     }
 
     @Override
     public String encode(byte[] bytes) {
-        return new String(base64.encode(bytes));
+        return new String(Base64.encodeBase64(bytes));
     }
 
 }
