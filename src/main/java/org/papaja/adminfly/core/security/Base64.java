43a1ab86e057382ffc020a80da93e1706c52a427
Ivan <ivan.gontarenko@gmail.com>
2019-07-26 18:19:07 +0300

    upd

--- src/main/java/org/papaja/adminfly/core/security/Base64.java
+++ src/main/java/org/papaja/adminfly/core/security/Base64.java
@@ -1,21 +1,19 @@
 package org.papaja.adminfly.core.security;
 
+import java.nio.charset.StandardCharsets;
+
+import static java.util.Base64.*;
+
 public class Base64 implements Encoder<String>, Decoder<String> {
-
-    private org.apache.commons.codec.binary.Base64 base64;
-
-    public Base64() {
-        base64 = new org.apache.commons.codec.binary.Base64();
-    }
 
     @Override
     public String decode(byte[] bytes) {
-        return new String(base64.decode(bytes));
+        return new String(getDecoder().decode(bytes));
     }
 
     @Override
     public String encode(byte[] bytes) {
-        return new String(base64.encode(bytes));
+        return new String(getEncoder().encode(bytes));
     }
 
 }
