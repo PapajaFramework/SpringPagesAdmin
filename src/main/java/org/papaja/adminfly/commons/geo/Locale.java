298b43785950e83593a9e475ff79a007f835add2
Ivan <ivan.gontarenko@gmail.com>
2019-08-08 19:31:26 +0300

    upd_win

--- src/main/java/org/papaja/adminfly/commons/geo/Locale.java
+++ src/main/java/org/papaja/adminfly/commons/geo/Locale.java
@@ -1,241 +1,184 @@
 package org.papaja.adminfly.commons.geo;
 
-import static java.util.Objects.nonNull;
-import static org.papaja.adminfly.commons.geo.Locale.Direction.LTR;
-import static org.papaja.adminfly.commons.geo.Locale.Direction.RTL;
+import static java.lang.String.format;
 
 @SuppressWarnings({"unused", "SpellCheckingInspection"})
 public enum Locale {
 
-    AF_ZA("af-ZA", "Afrikaans", "Afrikaans", Country.ZA),
-    AM_ET("am-ET", "Amharic", "ኣማርኛ", Country.ET, RTL),
-    AR_AE("ar-AE", "Arabic", "العربية", Country.AE, RTL),
-    AR_BH("ar-BH", "Arabic", "العربية", Country.BH, RTL),
-    AR_DZ("ar-DZ", "Arabic", "العربية", Country.DZ, RTL),
-    AR_EG("ar-EG", "Arabic", "العربية", Country.EG, RTL),
-    AR_IQ("ar-IQ", "Arabic", "العربية", Country.IQ, RTL),
-    AR_JO("ar-JO", "Arabic", "العربية", Country.JO, RTL),
-    AR_KW("ar-KW", "Arabic", "العربية", Country.KW, RTL),
-    AR_LB("ar-LB", "Arabic", "العربية", Country.LB, RTL),
-    AR_LY("ar-LY", "Arabic", "العربية", Country.LY, RTL),
-    AR_MA("ar-MA", "Arabic", "العربية", Country.MA, RTL),
-    AR_OM("ar-OM", "Arabic", "العربية", Country.OM, RTL),
-    AR_QA("ar-QA", "Arabic", "العربية", Country.QA, RTL),
-    AR_SA("ar-SA", "Arabic", "العربية", Country.SA, RTL),
-    AR_SY("ar-SY", "Arabic", "العربية", Country.SY, RTL),
-    AR_TN("ar-TN", "Arabic", "العربية", Country.TN, RTL),
-    AR_YE("ar-YE", "Arabic", "العربية", Country.YE, RTL),
-    AZ_AZ("az-AZ", "Azerbaijani", "Azərbaycan dili", Country.AZ),
-    BE_BY("be-BY", "Belarusian", "Беларуская", Country.BY),
-    BN_BD("bn-BD", "Bengali", "বাংলা", Country.BD),
-    BG_BG("bg-BG", "Bulgarian", "Български език", Country.BG),
-    BS_BA("bs-BA", "Bosnian", "Босански", Country.BA),
-    CA_ES("ca-ES", "Catalan", "Català", Country.ES),
-    CS_CZ("cs-CZ", "Czech", "Čeština", Country.CZ),
-    CY_GB("cy-GB", "Welsh", "Cymraeg", Country.GB),
-    DA_DK("da-DK", "Danish", "Dansk", Country.DK),
-    DE_AT("de-AT", "German", "Deutsch", Country.AT),
-    DE_CH("de-CH", "German", "Deutsch", Country.CH),
-    DE_DE("de-DE", "German", "Deutsch", Country.DE),
-    DE_LI("de-LI", "German", "Deutsch", Country.LI),
-    DE_LU("de-LU", "German", "Deutsch", Country.LU),
-    DV_MV("dv-MV", "Maldivian", "ދިވެހި", Country.MV),
-    EL_GR("el-GR", "Greek", "Ελληνικά", Country.GR),
-    EN_AU("en-AU", "English", "English", Country.AU),
-    EN_BZ("en-BZ", "English", "English", Country.BZ),
-    EN_CA("en-CA", "English", "English", Country.CA),
-    EN_GB("en-GB", "English", "English", Country.GB),
-    EN_IE("en-IE", "English", "English", Country.IE),
-    EN_JM("en-JM", "English", "English", Country.JM),
-    EN_NZ("en-NZ", "English", "English", Country.NZ),
-    EN_PH("en-PH", "English", "English", Country.PH),
-    EN_TT("en-TT", "English", "English", Country.TT),
-    EN_US("en-US", "English", "English", Country.US),
-    EN_ZA("en-ZA", "English", "English", Country.ZA),
-    EN_ZW("en-ZW", "English", "English", Country.ZW),
-    ES_AR("es-AR", "Spanish", "Español", Country.AR),
-    ES_BO("es-BO", "Spanish", "Español", Country.BO),
-    ES_CL("es-CL", "Spanish", "Español", Country.CL),
-    ES_CO("es-CO", "Spanish", "Español", Country.CO),
-    ES_CR("es-CR", "Spanish", "Español", Country.CR),
-    ES_DO("es-DO", "Spanish", "Español", Country.DO),
-    ES_EC("es-EC", "Spanish", "Español", Country.EC),
-    ES_ES("es-ES", "Spanish", "Español", Country.ES),
-    ES_GT("es-GT", "Spanish", "Español", Country.GT),
-    ES_HN("es-HN", "Spanish", "Español", Country.HN),
-    ES_MX("es-MX", "Spanish", "Español", Country.MX),
-    ES_NI("es-NI", "Spanish", "Español", Country.NI),
-    ES_PA("es-PA", "Spanish", "Español", Country.PA),
-    ES_PE("es-PE", "Spanish", "Español", Country.PE),
-    ES_PR("es-PR", "Spanish", "Español", Country.PR),
-    ES_PY("es-PY", "Spanish", "Español", Country.PY),
-    ES_SV("es-SV", "Spanish", "Español", Country.SV),
-    ES_UY("es-UY", "Spanish", "Español", Country.UY),
-    ES_VE("es-VE", "Spanish", "Español", Country.VE),
-    ET_EE("et-EE", "Estonian", "Eesti", Country.EE),
-    EU_ES("eu-ES", "Basque", "Euskara", Country.ES),
-    FA_IR("fa-IR", "Farsi", "فارسی", Country.IR),
-    FI_FI("fi-FI", "Finnish", "Suomi", Country.FI),
-    FO_FO("fo-FO", "Faroese", "Føroyskt", Country.FO),
-    FR_BE("fr-BE", "French", "Français", Country.BE),
-    FR_CA("fr-CA", "French", "Français", Country.CA),
-    FR_CH("fr-CH", "French", "Français", Country.CH),
-    FR_FR("fr-FR", "French", "Français", Country.FR),
-    FR_LU("fr-LU", "French", "Français", Country.LU),
-    FR_MC("fr-MC", "French", "Français", Country.MC),
-    GL_ES("gl-ES", "Galician", "Galego", Country.ES),
-    GU_IN("gu-IN", "Gujarati", "ગુજરાતી", Country.IN),
-    HE_IL("he-IL", "Hebrew", "עברית", Country.IL),
-    HI_IN("hi-IN", "Hindi", "फिजी बात", Country.IN),
-    HR_BA("hr-BA", "Croatian", "Hrvatski", Country.BA),
-    HR_HR("hr-HR", "Croatian", "Hrvatski", Country.HR),
-    HU_HU("hu-HU", "Hungarian", "Magyar", Country.HU),
-    HY_AM("hy-AM", "Armenian", "Հայերեն", Country.AM),
-    ID_ID("id-ID", "Indonesian","Bahasa Indonesia", Country.ID),
-    IS_IS("is-IS", "Icelandic", "Íslenska", Country.IS),
-    IT_CH("it-CH", "Italian", "Italiano", Country.CH),
-    IT_IT("it-IT", "Italian", "Italiano", Country.IT),
-    JA_JP("ja-JP", "Japanese", "日本語", Country.JP),
-    KA_GE("ka-GE", "Georgian", "ქართული", Country.GE),
-    KM_KH("km-KH", "Khmer", "ភាសាខ្មែរ", Country.KH),
-    KK_KZ("kk-KZ", "Kazakh", "Қазақ Tілі", Country.KZ),
-    KN_IN("kn-IN", "Kannada", "ಕನ್ನಡ", Country.IN),
-    KO_KR("ko-KR", "Korean", "한국어", Country.KR),
-    KOK_IN("kok-IN", "Konkani", "കൊങ്കണി", Country.IN),
-    KY_KG("ky-KG", "Kyrgyz", "قىرعىزچا", Country.KG),
-    LO_LA("lo-LA", "Lao", "ພາສາລາວ", Country.LA),
-    LT_LT("lt-LT", "Lithuanian", "Lietuvių", Country.LT),
-    LV_LV("lv-LV", "Latvian", "Latviešu", Country.LV),
-    MI_NZ("mi-NZ", "Maori", "Māori", Country.NZ),
-    MK_MK("mk-MK", "Macedonian", "Mакедонски", Country.MK),
-    MN_MN("mn-MN", "Mongolian", "Монгол Хэл", Country.MN),
-    MR_IN("mr-IN", "Marathi", "मराठी", Country.IN),
-    MS_BN("ms-BN", "Malay", "بهاس جاوي,", Country.BN),
-    MS_MY("ms-MY", "Malay", "بهاس جاوي,", Country.MY),
-    MT_MT("mt-MT", "Maltese", "Malti", Country.MT),
-    MY_MM("my-MM", "Burmese", "မြန်မာစာ", Country.MM),
-    NB_NO("nb-NO", "Norwegian", "Norsk", Country.NO),
-    NL_BE("nl-BE", "Dutch", "Dutch", Country.BE),
-    NL_NL("nl-NL", "Dutch", "Nederlands", Country.NL),
-    NS_ZA("ns-ZA", "Northern Sotho", "Sesotho sa Leboa", Country.ZA),
-    PA_IN("pa-IN", "Punjabi", "پنجابی", Country.IN),
-    PL_PL("pl-PL", "Polish", "Polski", Country.PL),
-    PS_AF("ps-AF", "Pashto", "پښتو", Country.AF),
-    PT_BR("pt-BR", "Portuguese", "Português", Country.BR),
-    PT_PT("pt-PT", "Portuguese", "Português", Country.PT),
-    QU_BO("qu-BO", "Quechua", "Runasimi", Country.BO),
-    QU_EC("qu-EC", "Quechua", "Runasimi", Country.EC),
-    QU_PE("qu-PE", "Quechua", "Runasimi", Country.PE),
-    RO_RO("ro-RO", "Romanian", "Roumanian", Country.RO),
-    RU_RU("ru-RU", "Russian", "Русский", Country.RU),
-    SA_IN("sa-IN", "Sanskrit", "संस्कृतम्", Country.IN),
-    SE_FI("se-FI", "Sami", "Sami", Country.FI),
-    SE_NO("se-NO", "Sami", "Sami", Country.NO),
-    SE_SE("se-SE", "Sami", "Sami", Country.SE),
-    SK_SK("sk-SK", "Slovak", "Slovakian", Country.SK),
-    SL_SI("sl-SI", "Slovenian", "Slovene", Country.SI),
-    SQ_AL("sq-AL", "Albanian", "Gjuha shqipe", Country.AL),
-    SR_BA("sr-BA", "Serbian", "Српски", Country.BA),
-    SV_FI("sv-FI", "Swedish", "Svenska", Country.FI),
-    SV_SE("sv-SE", "Swedish", "Svenska", Country.SE),
-    SW_KE("sw-KE", "Swahili", "Kiswahili", Country.KE),
-    SYR_SY("syr-SY", "Syriac", "ܠܫܢܐ ܣܘܪܝܝܐ", Country.SY, RTL),
-    TA_IN("ta-IN", "Tamil", "தமிழ்", Country.IN),
-    TE_IN("te-IN", "Telugu", "తెలుగు", Country.IN),
-    TH_TH("th-TH", "Thai", "ภาษาไทย", Country.TH),
-    TL_PH("tl-PH", "Tagalog", "Tagalog", Country.PH),
-    TN_ZA("tn-ZA", "Tswana", "Setswana", Country.ZA),
-    TR_TR("tr-TR", "Turkish", "Türkçe", Country.TR),
-    TT_RU("tt-RU", "Tatar", "Татарча", Country.RU),
-    UK_UA("uk-UA", "Ukrainian", "Українська", Country.UA),
-    UR_PK("ur-PK", "Urdu", "اُردُو", Country.PK, RTL),
-    UZ_UZ("uz-UZ", "Uzbek", "Oʻzbek ", Country.UZ),
-    VI_VN("vi-VN", "Vietnamese", "Tiếng", Country.VN),
-    XH_ZA("xh-ZA", "Xhosa", "Xhosa", Country.ZA),
-    ZH_CN("zh-CN", "Chinese", "汉语", Country.CN),
-    ZH_HK("zh-HK", "Chinese", "汉语", Country.HK),
-    ZH_MO("zh-MO", "Chinese", "汉语", Country.MO),
-    ZH_SG("zh-SG", "Chinese", "汉语", Country.SG),
-    ZH_TW("zh-TW", "Chinese", "汉语", Country.TW),
-    ZU_ZA("zu-ZA", "Zulu", "Zulu", Country.ZA);
+    AF_ZA(Language.AF, Country.ZA),
+    AM_ET(Language.AM, Country.ET),
+    AR_AE(Language.AR, Country.AE),
+    AR_BH(Language.AR, Country.BH),
+    AR_DZ(Language.AR, Country.DZ),
+    AR_EG(Language.AR, Country.EG),
+    AR_IQ(Language.AR, Country.IQ),
+    AR_JO(Language.AR, Country.JO),
+    AR_KW(Language.AR, Country.KW),
+    AR_LB(Language.AR, Country.LB),
+    AR_LY(Language.AR, Country.LY),
+    AR_MA(Language.AR, Country.MA),
+    AR_OM(Language.AR, Country.OM),
+    AR_QA(Language.AR, Country.QA),
+    AR_SA(Language.AR, Country.SA),
+    AR_SY(Language.AR, Country.SY),
+    AR_TN(Language.AR, Country.TN),
+    AR_YE(Language.AR, Country.YE),
+    AZ_AZ(Language.AZ, Country.AZ),
+    BE_BY(Language.BE, Country.BY),
+    BN_BD(Language.BN, Country.BD),
+    BG_BG(Language.BG, Country.BG),
+    BS_BA(Language.BS, Country.BA),
+    CA_ES(Language.CA, Country.ES),
+    CS_CZ(Language.CS, Country.CZ),
+    CY_GB(Language.CY, Country.GB),
+    DA_DK(Language.DA, Country.DK),
+    DE_AT(Language.DE, Country.AT),
+    DE_CH(Language.DE, Country.CH),
+    DE_DE(Language.DE, Country.DE),
+    DE_LI(Language.DE, Country.LI),
+    DE_LU(Language.DE, Country.LU),
+    DV_MV(Language.DV, Country.MV),
+    EL_GR(Language.EL, Country.GR),
+    EN_AU(Language.EN, Country.AU),
+    EN_BZ(Language.EN, Country.BZ),
+    EN_CA(Language.EN, Country.CA),
+    EN_GB(Language.EN, Country.GB),
+    EN_IE(Language.EN, Country.IE),
+    EN_JM(Language.EN, Country.JM),
+    EN_NZ(Language.EN, Country.NZ),
+    EN_PH(Language.EN, Country.PH),
+    EN_TT(Language.EN, Country.TT),
+    EN_US(Language.EN, Country.US),
+    EN_ZA(Language.EN, Country.ZA),
+    EN_ZW(Language.EN, Country.ZW),
+    ES_AR(Language.ES, Country.AR),
+    ES_BO(Language.ES, Country.BO),
+    ES_CL(Language.ES, Country.CL),
+    ES_CO(Language.ES, Country.CO),
+    ES_CR(Language.ES, Country.CR),
+    ES_DO(Language.ES, Country.DO),
+    ES_EC(Language.ES, Country.EC),
+    ES_ES(Language.ES, Country.ES),
+    ES_GT(Language.ES, Country.GT),
+    ES_HN(Language.ES, Country.HN),
+    ES_MX(Language.ES, Country.MX),
+    ES_NI(Language.ES, Country.NI),
+    ES_PA(Language.ES, Country.PA),
+    ES_PE(Language.ES, Country.PE),
+    ES_PR(Language.ES, Country.PR),
+    ES_PY(Language.ES, Country.PY),
+    ES_SV(Language.ES, Country.SV),
+    ES_UY(Language.ES, Country.UY),
+    ES_VE(Language.ES, Country.VE),
+    ET_EE(Language.ET, Country.EE),
+    EU_ES(Language.EU, Country.ES),
+    FA_IR(Language.FA, Country.IR),
+    FI_FI(Language.FI, Country.FI),
+    FO_FO(Language.FO, Country.FO),
+    FR_BE(Language.FR, Country.BE),
+    FR_CA(Language.FR, Country.CA),
+    FR_CH(Language.FR, Country.CH),
+    FR_FR(Language.FR, Country.FR),
+    FR_LU(Language.FR, Country.LU),
+    FR_MC(Language.FR, Country.MC),
+    GL_ES(Language.GL, Country.ES),
+    GU_IN(Language.GU, Country.IN),
+    HE_IL(Language.HE, Country.IL),
+    HI_IN(Language.HI, Country.IN),
+    HR_BA(Language.HR, Country.BA),
+    HR_HR(Language.HR, Country.HR),
+    HU_HU(Language.HU, Country.HU),
+    HY_AM(Language.HY, Country.AM),
+    ID_ID(Language.ID, Country.ID),
+    IS_IS(Language.IS, Country.IS),
+    IT_CH(Language.IT, Country.CH),
+    IT_IT(Language.IT, Country.IT),
+    JA_JP(Language.JA, Country.JP),
+    KA_GE(Language.KA, Country.GE),
+    KM_KH(Language.KM, Country.KH),
+    KK_KZ(Language.KK, Country.KZ),
+    KN_IN(Language.KN, Country.IN),
+    KO_KR(Language.KO, Country.KR),
+    KOK_IN(Language.KOK, Country.IN),
+    KY_KG(Language.KY, Country.KG),
+    LO_LA(Language.LO, Country.LA),
+    LT_LT(Language.LT, Country.LT),
+    LV_LV(Language.LV, Country.LV),
+    MI_NZ(Language.MI, Country.NZ),
+    MK_MK(Language.MK, Country.MK),
+    MN_MN(Language.MN, Country.MN),
+    MR_IN(Language.MR, Country.IN),
+    MS_BN(Language.MS, Country.BN),
+    MS_MY(Language.MS, Country.MY),
+    MT_MT(Language.MT, Country.MT),
+    MY_MM(Language.MY, Country.MM),
+    NB_NO(Language.NB, Country.NO),
+    NL_BE(Language.NL, Country.BE),
+    NL_NL(Language.NL, Country.NL),
+    NS_ZA(Language.NS, Country.ZA),
+    PA_IN(Language.PA, Country.IN),
+    PL_PL(Language.PL, Country.PL),
+    PS_AF(Language.PS, Country.AF),
+    PT_BR(Language.PT, Country.BR),
+    PT_PT(Language.PT, Country.PT),
+    QU_BO(Language.QU, Country.BO),
+    QU_EC(Language.QU, Country.EC),
+    QU_PE(Language.QU, Country.PE),
+    RO_RO(Language.RO, Country.RO),
+    RU_RU(Language.RU, Country.RU),
+    SA_IN(Language.SA, Country.IN),
+    SE_FI(Language.SE, Country.FI),
+    SE_NO(Language.SE, Country.NO),
+    SE_SE(Language.SE, Country.SE),
+    SK_SK(Language.SK, Country.SK),
+    SL_SI(Language.SL, Country.SI),
+    SQ_AL(Language.SQ, Country.AL),
+    SR_BA(Language.SR, Country.BA),
+    SV_FI(Language.SV, Country.FI),
+    SV_SE(Language.SV, Country.SE),
+    SW_KE(Language.SW, Country.KE),
+    SYR_SY(Language.SYR, Country.SY),
+    TA_IN(Language.TA, Country.IN),
+    TE_IN(Language.TE, Country.IN),
+    TH_TH(Language.TH, Country.TH),
+    TL_PH(Language.TL, Country.PH),
+    TN_ZA(Language.TN, Country.ZA),
+    TR_TR(Language.TR, Country.TR),
+    TT_RU(Language.TT, Country.RU),
+    UK_UA(Language.UK, Country.UA),
+    UR_PK(Language.UR, Country.PK),
+    UZ_UZ(Language.UZ, Country.UZ),
+    VI_VN(Language.VI, Country.VN),
+    XH_ZA(Language.XH, Country.ZA),
+    ZH_CN(Language.ZH, Country.CN),
+    ZH_HK(Language.ZH, Country.HK),
+    ZH_MO(Language.ZH, Country.MO),
+    ZH_SG(Language.ZH, Country.SG),
+    ZH_TW(Language.ZH, Country.TW),
+    ZU_ZA(Language.ZU, Country.ZA);
 
-    private Direction direction;
-    private String name;
-    private String local;
-    private String code;
-    private Country country;
+    private Language language;
+    private Country  country;
 
-    Locale(String code, String name) {
-        this(code, name, null, null, LTR);
-    }
-
-    Locale(String code, String name, String local) {
-        this(code, name, local, null, LTR);
-    }
-
-    Locale(String code, String name, Country country) {
-        this(code, name, null, country, LTR);
-    }
-
-    Locale(String code, String name, Direction direction) {
-        this(code, name, null, null, direction);
-    }
-
-    Locale(String code, String name, String local, Country country) {
-        this(code, name, local, country, LTR);
-    }
-
-    Locale(String code, String name, Country country, Direction direction) {
-        this(code, name, null, country, direction);
-    }
-
-    Locale(String code, String name, String local, Direction direction) {
-        this(code, name, local, null, direction);
-    }
-
-    Locale(String code, String name, String local, Country country, Direction direction) {
-        this.direction = direction;
-        this.code = code;
-        this.local = local;
-        this.name = name;
+    Locale(Language language, Country country) {
+        this.language = language;
         this.country = country;
     }
 
-    public boolean isRTL() {
-        return nonNull(direction) && direction.equals(RTL);
-    }
-
-    public boolean isLTR() {
-        return nonNull(direction) && direction.equals(LTR);
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public String getLocal() {
-        return local;
+    public Language getLanguage() {
+        return language;
     }
 
     public Country getCountry() {
         return country;
     }
 
-    public Direction getDirection() {
-        return direction;
+    public String getCode() {
+        return format("%s-%s", language.getCode(), country.getCode());
     }
 
     @Override
     public String toString() {
-        return String.format("Language{name='%s', local = '%s' code='%s'}", name, local, code);
+        return format("Locale{language=%s, country=%s}", language, country);
     }
-
-    public enum Direction {
-        RTL, LTR
-    }
-
 }
