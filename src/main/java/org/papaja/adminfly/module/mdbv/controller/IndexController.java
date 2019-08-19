40868a90352216bee99de0210ccf339141ee7875
Ivan Hontarenko <dezz@macbookPro.local>
2019-08-19 09:40:19 +0300

    olololo

--- src/main/java/org/papaja/adminfly/module/mdbv/controller/IndexController.java
+++ src/main/java/org/papaja/adminfly/module/mdbv/controller/IndexController.java
@@ -1,16 +1,20 @@
-package org.papaja.adminfly.module.wsa.controller;
+package org.papaja.adminfly.module.mdbv.controller;
 
 import org.papaja.adminfly.shared.controller.AbstractController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller("wsaIndex")
-@RequestMapping("/wsa")
+@RequestMapping("/mdbv")
 public class IndexController extends AbstractController {
 
-    @ResponseBody
+    @RequestMapping
+    public RedirectView home() {
+        return newRedirect("index");
+    }
+
     @RequestMapping("/index")
     public ModelAndView index() {
         ModelAndView mav = newView("index/index");
