00f651b38a8d0a572c85b7399d0a521e6df18c65
Ivan <ivan.gontarenko@gmail.com>
2019-08-27 19:11:37 +0300

    upd_win

--- src/main/java/org/papaja/adminfly/module/mdbv/common/manager/MongoDatabaseManager.java
+++ src/main/java/org/papaja/adminfly/module/mdbv/common/manager/MongoDatabaseManager.java
@@ -8,24 +8,22 @@ import java.util.Map;
 
 public class MongoDatabaseManager {
 
-    private ThreadLocal<Map<String, MongoTemplate>> local;
-    private MongoClient                             client;
+    private Map<String, MongoTemplate> templates;
+    private MongoClient                client;
 
     public MongoDatabaseManager(MongoClient client) {
-        this.client = client;
-        this.local  = new ThreadLocal<>();
-        this.local.set(new HashMap<>());
+        this.client     = client;
+        this.templates  = new HashMap<>();
     }
 
     public MongoTemplate getMongoTemplateForDatabase(String database) {
-        boolean isNew = !local.get().containsKey(database);
+        boolean isNew = !templates.containsKey(database);
 
         if (isNew) {
-            local.get().put(database, new MongoTemplate(client, database));
+            templates.put(database, new MongoTemplate(client, database));
         }
 
-        return local.get().get(database);
+        return templates.get(database);
     }
-
 
 }
